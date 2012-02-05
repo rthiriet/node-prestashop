205e11ca91b932ea3be2cb1cb63bb54da35a12ed
Rdmoshpit <thiriet@fullsix.com>
2012-02-06 00:29:54 +0100

    return a list of products

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,12 @@ exports.index = function(req, res){
 exports.api= function(req,res){
     var prestashopInst = new prestashop();
     var products = prestashopInst.productList();
+    console.log('productList requested')
     products
         .on('productListReceived',function(data){
             //res.render('test2',{ title: 'test'});
-            res.send(data.products);
+        console.log('about to go to render');
+            res.send(data);
                 })
         .on('error',function(data){
             res.send(data);
