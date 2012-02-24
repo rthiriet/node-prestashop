736139f00ba5e7af131f6d0a17945a7ebb5881d1
Rdmoshpit <thiriet@fullsix.com>
2012-02-24 16:12:24 +0100

    try to fix "Error R11"

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ app.post('/',function(request,response){
 });
 
 
-
-app.listen(3868);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
