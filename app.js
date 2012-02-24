77a00c12fc0e07ff1265f99fd1d5845e2647424c
Rdmoshpit <thiriet@fullsix.com>
2012-02-24 16:09:47 +0100

    now listens to 3868

--- app.js
+++ app.js
@@ -84,5 +84,5 @@ app.post('/',function(request,response){
 
 
 
-app.listen(3000);
+app.listen(3868);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
